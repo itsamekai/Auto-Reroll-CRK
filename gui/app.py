ee3ed550ee218e4b58288cc26b61ceffc32a2409
itsamekai <resayz4@ymail.com>
2025-08-01 00:10:07 +0800

    UI cleanup, added tabs and instructions

--- gui/app.py
+++ gui/app.py
@@ -12,41 +12,69 @@ LINE_COUNTS = [2, 3, 4]
 
 delay = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
 
+instructions = [
+    "1. Turn on CRK with Google Play Games. (Ensure CRK is in EN language)",
+    "2. Go to beascuit roll page.",
+    "3. Disable warning pop-ups on beascuit warnings.",
+    "4. Modify settings accordingly in settings tab.",
+    "5. Start button to start. ESC key to stop."
+    ]
+
 # probably not a clean solution
 # added so I can just move the elements around in the array to reorder the UI
 widgets = ["RollType", "RollCount", "Tainted", "Delay", "start", "log", "credit_name", "credit_server"]
 
-
-def create_widgets(app, on_start_callback):
-    tk.Label(app, text="Roll Type:").grid(row=widgets.index("RollType"), column=0, padx=10, pady=5)
-    roll_var = tk.StringVar(value=ROLL_TYPES[0])
-    ttk.Combobox(app, textvariable=roll_var, values=ROLL_TYPES, state="readonly").grid(row=widgets.index("RollType"), column=1)
-
-    tk.Label(app, text="How many rolls?").grid(row=widgets.index("RollCount"), column=0, padx=10, pady=5)
-    line_var = tk.StringVar(value=LINE_COUNTS[0])
-    ttk.Combobox(app, textvariable=line_var, values=LINE_COUNTS, state="readonly").grid(row=widgets.index("RollCount"), column=1)
-
-    tk.Label(app, text="Tainted Biscuit?").grid(row=widgets.index("Tainted"), column=0, padx=10, pady=5)
-    tainted_var = tk.StringVar(value="Disabled")
-    ttk.Combobox(app, textvariable=tainted_var, values=["Enabled", "Disabled"], state="readonly").grid(row=widgets.index("Tainted"), column=1)
-
-    tk.Label(app, text="Add Delay").grid(row=widgets.index("Delay"), column=0, padx=10, pady=5)
-    delay_var = tk.StringVar(value=delay[0])
-    ttk.Combobox(app, textvariable=delay_var, values=delay, state="readonly").grid(row=widgets.index("Delay"), column=1)    
-
-    start_btn = tk.Button(app, text="Start", command=on_start_callback)
+# widgets for 'Reroll' tab
+def createRerollWidgets(mainTab, on_start_callback):
+    start_btn = tk.Button(mainTab, text="Start", command=on_start_callback)
     start_btn.grid(row=widgets.index("start"), column=0, columnspan=2, pady=10)
 
-    tk.Label(app, text="Log Output:").grid(row=widgets.index("log"), column=0, columnspan=2)
-    log_box = tk.Text(app, height=12, width=75, state='disabled', wrap='word')
+    tk.Label(mainTab, text="Log Output:").grid(row=widgets.index("log"), column=0, columnspan=2)
+    log_box = tk.Text(mainTab, height=12, width=75, state='disabled', wrap='word')
     log_box.grid(row=widgets.index("log"), column=0, columnspan=2, padx=10, pady=5)
 
-    scrollbar = tk.Scrollbar(app, command=log_box.yview)
+    scrollbar = tk.Scrollbar(mainTab, command=log_box.yview)
     log_box.config(yscrollcommand=scrollbar.set)
     scrollbar.grid(row=widgets.index("log"), column=2, sticky='ns')
 
-    tk.Label(app, text="done by: Kai | discord: @boonkai", font=("Arial", 11)).grid(row=widgets.index("credit_name"), column=0, columnspan=2, sticky='w', padx=10)
-    tk.Label(app, text="discord: discord.gg/creamery", font=("Arial", 11)).grid(row=widgets.index("credit_server"), column=0, sticky='w', padx=10)
+    tk.Label(mainTab, text="done by: Kai | discord: @boonkai", font=("Arial", 11)).grid(row=widgets.index("credit_name"), column=0, columnspan=2, sticky='w', padx=10)
+    tk.Label(mainTab, text="discord: discord.gg/creamery", font=("Arial", 11)).grid(row=widgets.index("credit_server"), column=0, sticky='w', padx=10)
+
+    return log_box, start_btn 
+
+
+# widgets for 'Settings' tab   
+def createSettingsWidgets(settingsTab):
+    tk.Label(settingsTab, text="Roll Type:").grid(row=widgets.index("RollType"), column=0, padx=10, pady=5)
+    roll_var = tk.StringVar(value=ROLL_TYPES[0])
+    ttk.Combobox(settingsTab, textvariable=roll_var, values=ROLL_TYPES, state="readonly").grid(row=widgets.index("RollType"), column=1)
+
+    tk.Label(settingsTab, text="How many rolls?").grid(row=widgets.index("RollCount"), column=0, padx=10, pady=5)
+    line_var = tk.StringVar(value=LINE_COUNTS[0])
+    ttk.Combobox(settingsTab, textvariable=line_var, values=LINE_COUNTS, state="readonly").grid(row=widgets.index("RollCount"), column=1)
+
+    tk.Label(settingsTab, text="Tainted Biscuit?").grid(row=widgets.index("Tainted"), column=0, padx=10, pady=5)
+    tainted_var = tk.StringVar(value="Disabled")
+    ttk.Combobox(settingsTab, textvariable=tainted_var, values=["Enabled", "Disabled"], state="readonly").grid(row=widgets.index("Tainted"), column=1)
+
+    tk.Label(settingsTab, text="Add Delay").grid(row=widgets.index("Delay"), column=0, padx=10, pady=5)
+    delay_var = tk.StringVar(value=delay[0])
+    ttk.Combobox(settingsTab, textvariable=delay_var, values=delay, state="readonly").grid(row=widgets.index("Delay"), column=1)  
+
+    return roll_var, line_var, tainted_var, delay_var
+
+
+# widgets for 'Instructions' tab
+def createInstructionsWidgets(instructionsTab):
+    for i, inst in enumerate(instructions):
+        label = tk.Label(instructionsTab, text=inst, anchor="w", justify="left")
+        label.grid(row=i, column=0, sticky="w", padx=10, pady=5)
+
+# init all widgets and return
+def create_widgets(mainTab, settingsTab, instructionsTab, on_start_callback):
+    log_box, start_btn = createRerollWidgets(mainTab, on_start_callback)
+    roll_var, line_var, tainted_var, delay_var = createSettingsWidgets(settingsTab)
+    createInstructionsWidgets(instructionsTab)
 
     return {
         "roll_var": roll_var,
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from gui.app import create_widgets
 from gui.event import on_start, start_listener
 from utils.paths import set_tesseract_path
@@ -11,6 +12,21 @@ def main():
     app = tk.Tk()
     app.title("Auto Reroll by Kai")
 
+    style = ttk.Style()
+    style.configure("TNotebook.Tab", padding=[10, 5])
+    # tabs for setting / main page
+    notebook = ttk.Notebook(app)
+    notebook.pack(padx=10, pady=10, expand=True, fill='both')
+
+    mainTab = ttk.Frame(notebook, padding=10)
+    notebook.add(mainTab, text="Reroll")
+
+    settingsTab = ttk.Frame(notebook, padding=10)
+    notebook.add(settingsTab, text="Settings")
+
+    instructionsTab = ttk.Frame(notebook, padding=10)
+    notebook.add(instructionsTab, text="Instructions")
+
     # for logging to logbox in UI
     def log(msg):
         log_box.config(state='normal')
@@ -22,7 +38,7 @@ def main():
     def start_callback():
         on_start(widgets["roll_var"], widgets["line_var"], widgets["tainted_var"], widgets["delay_var"], log, tesseractAPI)
 
-    widgets = create_widgets(app, start_callback)
+    widgets = create_widgets(mainTab, settingsTab, instructionsTab, start_callback)
     log_box = widgets["log_box"]
     # start esc listener
     start_listener(log)
